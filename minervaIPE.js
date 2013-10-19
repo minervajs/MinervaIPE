68dc6c80e8337b95d1ba80018f95e2eebb03b02d
David Robbins <robbins.david@gmail.com>
2013-10-19 13:37:46 -0500

    Create minervaIPE.js

---
+++ minervaIPE.js
@@ -0,0 +1,6 @@
+IPE.ui.registerTab({
+    id: "minerva",
+    title: "MinervaJS",
+    content: "<iframe src=\"http://minervajs.org/site/index.html\"></iframe>",
+    switchTab: true
+}, function () {})
