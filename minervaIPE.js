dddbe28d9585fba698a27069dc78d3d6d1743cfd
David Robbins <robbins.david@gmail.com>
2013-10-19 14:03:07 -0500

    Improve appearance of the iFrame

--- minervaIPE.js
+++ minervaIPE.js
@@ -1,6 +1,6 @@
 IPE.ui.registerTab({
     id: "minerva",
     title: "MinervaJS",
-    content: "<iframe src=\"http://minervajs.org/site/index.html\"></iframe>",
+    content: "<iframe src=\"http://minervajs.org/site/index.html\" width=\"100%\" height=\"600px\" seamless></iframe>",
     switchTab: true
 }, function () {})
